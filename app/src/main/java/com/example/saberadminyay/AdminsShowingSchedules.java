05df0128713ec1941964f624988929134cfb051f
zainab <zainabsultan1111@gmail.com>
2019-12-06 14:12:16 +0200

    initial commit

---
+++ app/src/main/java/com/example/saberadminyay/AdminsShowingSchedules.java
@@ -0,0 +1,16 @@
+package com.example.saberadminyay;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class test extends AppCompatActivity {
+Button n;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+        n= (Button) findViewById(R.id.n);
+    }
+}
