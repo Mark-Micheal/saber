d98f654b103a0264ed209b6b09a92573ae8fbd30
fadi-saad <fadoxin@gmail.com>
2019-12-08 19:48:57 +0200

    added few stuff

--- AndroidFront/saberFront/app/src/main/java/com/example/saberfront/reservationActivity.java
+++ AndroidFront/saberFront/app/src/main/java/com/example/saberfront/reservationActivity.java
@@ -20,9 +20,23 @@ import android.view.View;
 
 public class reservationActivity extends AppCompatActivity {
 
+
+    String token;
+    Spinner dropdown1;
+    Spinner dropdown2;
+    Spinner dropdown3;
+    Spinner dropdown4;
+    Spinner dropdown5;
+
     public void SearchForFree(View view){
-        //startActivity(new Intent(getApplicationContext(),resultsActivity.class));
-        //api to backend to get free rooms at said filters.
+        Intent results = new Intent(getApplicationContext(),slotsResults.class);
+        results.putExtra("token",token);
+        results.putExtra("day",dropdown1.getSelectedItem().toString());
+        results.putExtra("slot",dropdown1.getSelectedItem().toString());
+        results.putExtra("building",dropdown1.getSelectedItem().toString());
+        results.putExtra("floor",dropdown1.getSelectedItem().toString());
+        results.putExtra("room",dropdown1.getSelectedItem().toString());
+        startActivity(results);
     }
 
     @Override
@@ -30,27 +44,29 @@ public class reservationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
 
-        Spinner dropdown1 = findViewById(R.id.spinner1);
+        token = getIntent().getStringExtra("token");
+
+        dropdown1 = findViewById(R.id.spinner1);
         String[] items1 = new String[]{"Choose a day","Monday", "Tuesday", "Wednesday","Thursday","Friday","Saturday","Sunday"};
         ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items1);
         dropdown1.setAdapter(adapter1);
 
-        Spinner dropdown2 = findViewById(R.id.spinner2);
+        dropdown2 = findViewById(R.id.spinner2);
         String[] items2 = new String[]{"Choose a slot","1st", "2nd", "3rd","4th","5th","After Hours"};
         ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items2);
         dropdown2.setAdapter(adapter2);
 
-        Spinner dropdown3 = findViewById(R.id.spinner);
+        dropdown3 = findViewById(R.id.spinner);
         String[] items3 = new String[]{"Choose a building","B", "C", "D"};
         ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items3);
         dropdown3.setAdapter(adapter3);
 
-        Spinner dropdown4 = findViewById(R.id.spinner3);
+        dropdown4 = findViewById(R.id.spinner3);
         String[] items4 = new String[]{"Choose a floor","1", "2", "3"};
         ArrayAdapter<String> adapter4 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items4);
         dropdown4.setAdapter(adapter4);
 
-        Spinner dropdown5 = findViewById(R.id.spinner4);
+        dropdown5 = findViewById(R.id.spinner4);
         String[] items5 = new String[]{"Choose a room","01", "02", "03","04", "05", "06","07", "08", "09","10", "11", "12"};
         ArrayAdapter<String> adapter5 = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items5);
         dropdown5.setAdapter(adapter5);
---
+++ AndroidFront/saberFront/app/src/main/java/com/example/saberfront/slotsResults.java
@@ -0,0 +1,122 @@
+package com.example.saberfront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class slotsResults extends AppCompatActivity {
+
+    String token;
+
+    ListView ls;
+    private RequestQueue mQ;
+    public ArrayList<String> content = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slots_results);
+        token = getIntent().getStringExtra("token");
+        ls = (ListView) findViewById(R.id.listView2);
+        mQ = Volley.newRequestQueue(this);
+        getFree();
+
+
+
+        Map<String, String> params = new HashMap<String, String>();
+//        params.put("email",email.getText().toString());
+//        params.put("password",password.getText().toString());
+        JSONObject myparams = new JSONObject(params);
+        JsonObjectRequest jsonObjReq =new JsonObjectRequest(Request.Method.POST,
+                "https://saberapp.herokuapp.com/api/login", myparams,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+
+                        } catch (JSONException e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }}){
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type","application/json");
+                headers.put("Accept","application/json");
+                return headers;
+            }
+        };
+        mQ.add(jsonObjReq);
+    }
+
+    void getFree(){
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET,
+                "https://saberapp.herokuapp.com/api/myReservations", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                String res = "";
+                                JSONObject element = response.getJSONObject(i);
+                                res += element.getString("day") + " , " + element.getString("slot");
+                                content.add(res);
+                            }
+                            ArrayAdapter<String> aa = new ArrayAdapter<String>(slotsResults.this, android.R.layout.simple_list_item_1, content);
+                            ls.setAdapter(aa);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error Occured");
+            }
+        }) {
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Accept", "application/json");
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        mQ.add(req);
+    }
+}
