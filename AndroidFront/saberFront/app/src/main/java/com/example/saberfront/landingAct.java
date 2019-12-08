e6c327a998c18dcd1006a2aa41c54e1444360768
fadi-saad <fadoxin@gmail.com>
2019-12-08 12:19:21 +0200

    integrated login, register and get my reservations, will hove some changes by 7~8pm today

--- AndroidFront/saberFront/app/src/main/java/com/example/saberfront/landingAct.java
+++ AndroidFront/saberFront/app/src/main/java/com/example/saberfront/landingAct.java
@@ -7,17 +7,25 @@ import android.os.Bundle;
 import android.view.View;
 
 public class landingAct extends AppCompatActivity {
+    String token;
 
     public void reserve(View view){
-        startActivity(new Intent(getApplicationContext(),reservationActivity.class));
+        Intent reserveInt = new Intent(getApplicationContext(),reservationActivity.class);
+        reserveInt.putExtra("token",token);
+        startActivity(reserveInt);
+
     }
 
     public void getMyReservations(View view){
-
+        Intent myRes = new Intent(getApplicationContext(),viewMyRes.class);
+        myRes.putExtra("token",token);
+        startActivity(myRes);
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        token = getIntent().getStringExtra("token");
     }
 }
---
+++ AndroidFront/saberFront/app/src/main/java/com/example/saberfront/viewMyRes.java
@@ -0,0 +1,86 @@
+package com.example.saberfront;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class viewMyRes extends AppCompatActivity {
+
+    String token;
+    ListView ls;
+    private RequestQueue mQ;
+    public ArrayList<String> content = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_my_res);
+        token = getIntent().getStringExtra("token");
+        ls = (ListView) findViewById(R.id.listView);
+        mQ = Volley.newRequestQueue(this);
+        requestMyReservations();
+    }
+
+    void requestMyReservations(){
+        JsonArrayRequest req = new JsonArrayRequest(Request.Method.GET,
+                "https://saberapp.herokuapp.com/api/myReservations", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        try {
+                            for (int i = 0; i < response.length(); i++) {
+                                String res = "";
+                                JSONObject element = response.getJSONObject(i);
+                                res += element.getString("day") + " , " + element.getString("slot");
+                                content.add(res);
+                            }
+                            ArrayAdapter<String> aa = new ArrayAdapter<String>(viewMyRes.this, android.R.layout.simple_list_item_1, content);
+                            ls.setAdapter(aa);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("Error Occured");
+            }
+        }) {
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Content-Type", "application/json");
+                headers.put("Accept", "application/json");
+                headers.put("Authorization", "Bearer " + token);
+                return headers;
+            }
+        };
+        mQ.add(req);
+    }
+}
